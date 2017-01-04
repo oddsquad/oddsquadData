b19654afbae74295d156516d002d7d10cc9e670c
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-01-04 11:25:59 -0700

    Test card pack validity

--- test/test.js
+++ test/test.js
@@ -2,35 +2,47 @@ var fs = require('fs');
 var YAML = require('yamljs');
 var async = require('async');
 
+function checkDir(dir, callback) {
+	dir = __dirname+"/../"+dir;
+	fs.readdir(dir, function(err, files) {
+		if(err) {
+			throw err;
+		}
+		var calls = [];
+		for(var i = 0; i < files.length; i++) {
+			if(files[i].indexOf(".yml") > -1) {
+				calls.push(function(file, callback) {
+					fs.readFile(dir+"/"+file, function(err, content) {
+						if(err) {
+							callback(err);
+							return;
+						}
+						try {
+							callback(null, YAML.parse(content.toString()));
+						} catch(e) {
+							console.log("!!Failed to parse \""+file+"\"!!");
+							callback(e);
+						}
+					})
+				}.bind(this, files[i]));
+			}
+		}
+		async.parallel(calls, callback);
+	});
+}
+
 describe('Card', function() {
 	describe('data', function() {
-		var dir = __dirname+"/../cards";
 		it('should be valid YAML', function(done) {
-			fs.readdir(dir, function(err, files) {
-				if(err) {
-					throw err;
-				}
-				var calls = [];
-				for(var i = 0; i < files.length; i++) {
-					if(files[i].indexOf(".yml") > -1) {
-						calls.push(function(file, callback) {
-							fs.readFile(dir+"/"+file, function(err, content) {
-								if(err) {
-									callback(err);
-									return;
-								}
-								try {
-									callback(null, YAML.parse(content.toString()));
-								} catch(e) {
-									console.log("!!Failed to parse \""+file+"\"!!");
-									callback(e);
-								}
-							})
-						}.bind(this, files[i]));
-					}
-				}
-				async.parallel(calls, done);
-			});
+			checkDir("cards", done);
+		});
+	});
+});
+
+describe('Card Packs', function() {
+	describe('data', function() {
+		it('should be valid YAML', function(done) {
+			checkDir("cardpacks", done);
 		});
 	});
 });
